609df124a1f72918a989ded6a5e5370eb16e4300
Josue1707 <josuemonro@gmail.com>
2025-01-14 11:38:13 -0600

    Solved conflicts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Importa los estilos de tu proyecto y Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'; // CSS de Bootstrap
 import 'bootstrap'; // JavaScript de Bootstrap
@@ -6,9 +5,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'vue3-toastify/dist/index.css';
 // import '@fortawesome/fontawesome-free/css/all.min.css'; // Estilos de Font Awesome
 // Importa Vue y las configuraciones de tu proyecto
-=======
-// main.js
->>>>>>> d0c667e285c2719f5bb0c8277bef4dfafe78374a
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -21,10 +17,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const app = createApp(App);
 
 app.use(router);
-<<<<<<< HEAD
-// Monta la aplicación
-=======
 app.use(store); // Usa Vuex store
 
->>>>>>> d0c667e285c2719f5bb0c8277bef4dfafe78374a
 app.mount('#app');
